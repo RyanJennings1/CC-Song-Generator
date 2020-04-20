96cb7ecf32a0c19c4ac04a4e62c1449991d1ef41
Ryan Jennings <ryanjennings1010@gmail.com>
2020-04-20 19:17:02 +0100

    Update styling and load latest checkpoint

--- rnn_play.py
+++ rnn_play.py
@@ -29,41 +29,54 @@ lyrics21 = 'checkpoints/rnn_train_1586614351-2100000'
 lyrics30 = 'checkpoints/rnn_train_1586635708-3000000'
 lyrics42 = 'checkpoints/rnn_train_1586649298-4200000'
 
+# need a better for getting the latests
 author = lyrics42
 
-ncnt = 0
-with tf.Session() as sess:
-    new_saver = tf.train.import_meta_graph('checkpoints/rnn_train_1586649298-4200000.meta')
+def main():
+  """
+  main method
+  """
+  ncnt = 0
+  with tf.Session() as sess:
+    checkpoint_dir: str = './checkpoints'
+    #new_saver = tf.train.import_meta_graph('checkpoints/rnn_train_1586649298-4200000.meta')
+    new_saver = tf.train.import_meta_graph(f'{tf.train.latest_checkpoint(checkpoint_dir)}.meta')
     new_saver.restore(sess, author)
     x = txtutil.convert_from_alphabet(ord("L"))
     x = np.array([[x]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
 
     # initial values
     y = x
-    h = np.zeros([1, INTERNALSIZE * NLAYERS], dtype=np.float32)  # [ BATCHSIZE, INTERNALSIZE * NLAYERS]
-    file = open("generated_output.txt", "w")
-    file.write("Hi there, this a generated output song from the lyric machine. have fun! \n\n")
-    for i in range(10000):
-        yo, h = sess.run(['Yo:0', 'H:0'], feed_dict={'X:0': y, 'pkeep:0': 1., 'Hin:0': h, 'batchsize:0': 1})
+    # [ BATCHSIZE, INTERNALSIZE * NLAYERS]
+    h = np.zeros([1, INTERNALSIZE * NLAYERS], dtype=np.float32)
 
-        # If sampling is be done from the topn most likely characters, the generated text
-        # is more credible and more "english". If topn is not set, it defaults to the full
-        # distribution (ALPHASIZE)
+    output_file = open("generated_output.txt", "w")
+    for _ in range(140):
+      yo, h = sess.run(['Yo:0', 'H:0'],
+                       feed_dict={'X:0': y, 'pkeep:0': 1., 'Hin:0': h, 'batchsize:0': 1})
 
-        # Recommended: topn = 10 for intermediate checkpoints, topn=2 or 3 for fully trained checkpoints
+      # If sampling is be done from the topn most likely characters, the generated text
+      # is more credible and more "english". If topn is not set, it defaults to the full
+      # distribution (ALPHASIZE)
 
-        c = txtutil.sample_from_probabilities(yo, topn=2)
-        y = np.array([[c]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
-        c = chr(txtutil.convert_to_alphabet(c))
-        print(c, end="")
-        file.write(c)
+      # Recommended: topn = 10 for intermediate checkpoints,
+      #   topn = 2 or 3 for fully trained checkpoints
 
-        if c == '\n':
-            ncnt = 0
-        else:
-            ncnt += 1
-        if ncnt == 100:
-            print("")
-            file.write("")
-            ncnt = 0
-    file.close() 
+      next_char = txtutil.sample_from_probabilities(yo, topn=2)
+      y = np.array([[next_char]])  # shape [BATCHSIZE, SEQLEN] with BATCHSIZE=1 and SEQLEN=1
+      next_char = chr(txtutil.convert_to_alphabet(next_char))
+      print(next_char, end="")
+      output_file.write(next_char)
+
+      if next_char == '\n':
+        ncnt = 0
+      else:
+        ncnt += 1
+      if ncnt == 100:
+        print("")
+        output_file.write("")
+        ncnt = 0
+    output_file.close()
+
+if __name__ == "__main__":
+  main()
